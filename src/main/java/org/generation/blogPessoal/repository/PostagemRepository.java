0b347e8554d05deb18d44048e8aa282ed3bfab05
Murilo <murilopaulino@live.com>
2021-03-19 17:09:31 -0300

    camadas TemaController e TemaRepository e seus respectivos métodos

--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{		//jpa hibernate
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{			//jpa hibernate
 																					// titulo --> Titulo (em razão do camelCase)
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);		//"Containing" é mais ou menos o "like" do MySQL
+
+	
 }
