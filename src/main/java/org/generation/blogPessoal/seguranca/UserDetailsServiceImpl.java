6b5dc045f887af9cd50d54623f38b99fa879f54a
Murilo <murilopaulino@live.com>
2021-04-06 15:53:43 -0300

    criação da camada de serviço e UsuarioController

---
+++ src/main/java/org/generation/blogPessoal/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package org.generation.blogPessoal.seguranca;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	private List<GrantedAuthority> authorities;
+	
+	
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+	    return authorities;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername (String userName) throws UsernameNotFoundException{
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + "Not found."));
+		
+		return user.map(UserDetailsImpl::new).get();
+	}
+
+}
